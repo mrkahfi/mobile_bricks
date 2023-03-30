1acb5cd3ca17378fd1151117c8120d73bbb58f66
Kahfi <kahfi@zero-one-group.com>
2023-03-31 06:53:10 +0700

    chore: rm ios/Pods and ios/Podfile.lock

--- zot_starter_mobile/hooks/post_gen.dart
+++ zot_starter_mobile/hooks/post_gen.dart
@@ -11,21 +11,21 @@ void run(HookContext context) async {
       --project=$firebaseProjectId \\
       --ios-bundle-id=$iosBundleId.dev \\
       --android-app-id=$androidAppId.dev \\
-      --out=lib/src/app/firebase/firebase_options_dev.dart \\
+      --out=lib/src/app/firebase/firebase_options_dev.dart \
   ''');
   context.logger.info('''
   flutterfire config \\
       --project=$firebaseProjectId \\
       --ios-bundle-id=$iosBundleId.stg \\
       --android-app-id=$androidAppId.stg \\
-      --out=lib/src/app/firebase/firebase_options_stg.dart \\
+      --out=lib/src/app/firebase/firebase_options_stg.dart \
   ''');
   context.logger.info('''
   flutterfire config \\
       --project=$firebaseProjectId \\
       --ios-bundle-id=$iosBundleId \\
       --android-app-id=$androidAppId \\
-      --out=lib/src/app/firebase/firebase_options.dart \\
+      --out=lib/src/app/firebase/firebase_options.dart \
   ''');
   context.logger
       .info('Or check ./README.md on how to configure Firebase projects');
