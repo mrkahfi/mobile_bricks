8ac822dc1fa304b8ea0ef23ca6bfc909922f4b44
Kahfi <kahfi@zero-one-group.com>
2023-03-31 06:42:21 +0700

    feat: udpated readme and brick.yaml

--- zot_starter_mobile/hooks/post_gen.dart
+++ zot_starter_mobile/hooks/post_gen.dart
@@ -7,25 +7,25 @@ void run(HookContext context) async {
   context.logger.info(
       'Please run following command to configure Firebase projects for all the environments');
   context.logger.info('''
-  flutterfire config \
-      --project=$firebaseProjectId \
-      --ios-bundle-id=$iosBundleId.dev \
-      --android-app-id=$androidAppId.dev \
-      --out=lib/src/app/firebase/firebase_options_dev.dart \
+  flutterfire config \\
+      --project=$firebaseProjectId \\
+      --ios-bundle-id=$iosBundleId.dev \\
+      --android-app-id=$androidAppId.dev \\
+      --out=lib/src/app/firebase/firebase_options_dev.dart \\
   ''');
   context.logger.info('''
-  flutterfire config \
-      --project=$firebaseProjectId \
-      --ios-bundle-id=$iosBundleId.stg \
-      --android-app-id=$androidAppId.stg \
-      --out=lib/src/app/firebase/firebase_options_stg.dart \
+  flutterfire config \\
+      --project=$firebaseProjectId \\
+      --ios-bundle-id=$iosBundleId.stg \\
+      --android-app-id=$androidAppId.stg \\
+      --out=lib/src/app/firebase/firebase_options_stg.dart \\
   ''');
   context.logger.info('''
-  flutterfire config \
-      --project=$firebaseProjectId \
-      --ios-bundle-id=$iosBundleId \
-      --android-app-id=$androidAppId \
-      --out=lib/src/app/firebase/firebase_options.dart \
+  flutterfire config \\
+      --project=$firebaseProjectId \\
+      --ios-bundle-id=$iosBundleId \\
+      --android-app-id=$androidAppId \\
+      --out=lib/src/app/firebase/firebase_options.dart \\
   ''');
   context.logger
       .info('Or check ./README.md on how to configure Firebase projects');
