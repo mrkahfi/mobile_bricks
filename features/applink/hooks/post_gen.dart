f372d527813cd473cae0ffbc09a94a0cb7b4710c
Muhammad Rijalul Kahfi <kahfi@zero-one-group.com>
2023-04-28 15:51:42 +0700

    fix: postgen applink

--- features/applink/hooks/post_gen.dart
+++ features/applink/hooks/post_gen.dart
@@ -8,13 +8,57 @@ void run(HookContext context) async {
   final devAppLink = context.vars['devAppLink'];
   final stgAppLink = context.vars['stgAppLink'];
 
+  final sha256 = context.vars['sha256'];
+  final devSha256 = context.vars['devSha256'];
+  final stgSha256 = context.vars['stgSha256'];
+
+  final appleTeamId = context.vars['appleTeamId'];
+
+  // Build.gradle
   var filePath = p.join(Directory.current.path, 'android/app', 'build.gradle');
   File file = File(filePath);
   String fileContent = await file.readAsString();
 
-  String newFileContent = fileContent.replaceAll(r'{{ appLink }}', appLink);
-  newFileContent = newFileContent.replaceAll(r'{{ devAppLink }}', devAppLink);
-  newFileContent = newFileContent.replaceAll(r'{{ stgAppLink }}', stgAppLink);
+  String newFileContent = fileContent.replaceAll(r'appLink', appLink);
+  newFileContent = newFileContent.replaceAll(r'devAppLink', devAppLink);
+  newFileContent = newFileContent.replaceAll(r'stgAppLink', stgAppLink);
 
   await file.writeAsString(newFileContent);
+
+  context.logger.info(''' 
+  $newFileContent
+  ''');
+
+  // assetlinks.json
+  var assetlinksPath =
+      p.join(Directory.current.path, 'android', 'assetlinks.json');
+  File assetLinksFile = File(assetlinksPath);
+  String assetLinksContent = await assetLinksFile.readAsString();
+
+  String newAssetLinksContent = assetLinksContent.replaceAll(r'sha256', sha256);
+  newAssetLinksContent =
+      newAssetLinksContent.replaceAll(r'devSha256', devSha256);
+  newAssetLinksContent =
+      newAssetLinksContent.replaceAll(r'stgSha256', stgSha256);
+
+  await assetLinksFile.writeAsString(newAssetLinksContent);
+
+  context.logger.info(''' 
+  $newAssetLinksContent
+  ''');
+
+  // apple-app-site-association
+  var siteAssocPath =
+      p.join(Directory.current.path, 'ios', 'apple-app-site-association');
+  File siteAssocFile = File(siteAssocPath);
+  String siteAssocContent = await siteAssocFile.readAsString();
+
+  String newSiteAssocContent =
+      siteAssocContent.replaceAll(r'appleTeamId', appleTeamId);
+
+  await siteAssocFile.writeAsString(newSiteAssocContent);
+
+  context.logger.info(''' 
+  $newSiteAssocContent 
+  ''');
 }
