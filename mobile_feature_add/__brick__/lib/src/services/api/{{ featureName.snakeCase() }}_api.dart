18cdfa8aec84674446e4c8ca861581a390f3366f
Muhammad Rijalul Kahfi <yanmi.inc@gmail.com>
2023-05-05 17:04:43 +0700

    feat: add api client in feature brick

--- mobile_feature_add/__brick__/lib/src/services/api/{{ featureName.snakeCase() }}_api.dart
+++ mobile_feature_add/__brick__/lib/src/services/api/{{ featureName.snakeCase() }}_api.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
 
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../src/features/{{ featureName.snakeCase() }}/data/models/responses/{{ featureName.snakeCase() }}_response.dart';
 import '../../../src/features/{{ featureName.snakeCase() }}/data/models/responses/{{ modelName.snakeCase() }}_response.dart';
 import '../../services/remote/config/config.dart';
 
@@ -15,11 +13,13 @@ class {{ featureName.pascalCase() }}Api {
 
   Future<ApiResponse<List<{{ modelName.pascalCase() }}Response>>> get{{ modelName.pascalCase() }}s() async {
     try {
-      final json = await _dioClient.get('{{ modelName.paramCase }}Endpoint') as String;
-      final response =
-          {{ featureName.pascalCase() }}Response.fromJson(jsonDecode(json) as Map<String, dynamic>);
+      
+      final json = await _dioClient.get<String>('{{ modelName.paramCase() }}-endpoint') as String;
+      final response = (jsonDecode(json) as List<Map<String, dynamic>>)
+          .map((e) => {{ modelName.pascalCase() }}Response.fromJson(e))
+          .toList();
 
-      return ApiResponse.success(response.items);
+      return ApiResponse.success(response);
     } on Exception catch (e, st) {
       return ApiResponse.failure(
         NetworkExceptions.getDioException(e, st),
