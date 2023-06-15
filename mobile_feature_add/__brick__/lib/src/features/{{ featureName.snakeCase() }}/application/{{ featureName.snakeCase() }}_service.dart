281256192fc77fd5c95a14ceab57b870f1e3404c
Muhammad Rijalul Kahfi <kahfi@zero-one-group.com>
2023-06-15 22:36:28 +0700

    refactor: make data and domain layer optional

--- mobile_feature_add/__brick__/lib/src/features/{{ featureName.snakeCase() }}/application/{{ featureName.snakeCase() }}_service.dart
+++ mobile_feature_add/__brick__/lib/src/features/{{ featureName.snakeCase() }}/application/{{ featureName.snakeCase() }}_service.dart
@@ -1,24 +1,19 @@
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../data/repositories/{{ featureName.snakeCase() }}_repository.dart';
-import '../domain/{{ featureName.snakeCase() }}_mapper.dart';
-import '../domain/entity/{{ entityName.snakeCase() }}.dart';
-import '../../../../src/services/remote/config/api_response.dart';
+import '../../../../src/data/services/remote/config/api_response.dart';
+import '../../../../src/domain/entities/item.dart';
 
 class {{ featureName.pascalCase() }}Service {
-  {{ featureName.pascalCase() }}Service(this._{{ featureName.camelCase() }}Repository);
+  {{ featureName.pascalCase() }}Service();
 
-  final {{ featureName.pascalCase() }}Repository _{{ featureName.camelCase() }}Repository;
 
-  Future<ApiResponse<List<{{ entityName.pascalCase() }}>>> fetch{{ entityName.pascalCase() }}s() async {
-    final response = await _{{ featureName.camelCase() }}Repository.fetch{{ modelName.pascalCase() }}s();
-    return {{ featureName.pascalCase() }}Mapper.map{{ modelName.pascalCase() }}ListResponseTo{{ entityName.pascalCase() }}List(response);
+  Future<ApiResponse<List<Item>>> doSomething() async {
+    return const ApiResponse.success([]);
   }
 }
 
+
 final {{ featureName.camelCase() }}ServiceProvider = Provider<{{ featureName.pascalCase() }}Service>((ref) {
-  return {{ featureName.pascalCase() }}Service(
-    ref.read({{ featureName.camelCase() }}RepositoryProvider),
-  );
+  return {{ featureName.pascalCase() }}Service();
 });
--- mobile_feature_add/hooks/post_gen.dart
+++ mobile_feature_add/hooks/post_gen.dart
@@ -4,11 +4,18 @@ import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
   context.logger.info(''' \ ''');
+  final fixResult = await Process.run('dart', [
+    'fix',
+    '--apply',
+  ]);
   context.logger.info(''' \
 Executing flutter pub run build_runner build --delete-conflicting-outputs    \
 ''');
-  var result = await Process.run('flutter',
+  final result = await Process.run('flutter',
       ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs']);
   stdout.write(result.stdout);
   stderr.write(result.stderr);
+
+  stdout.write(fixResult.stdout);
+  stderr.write(fixResult.stderr);
 }
